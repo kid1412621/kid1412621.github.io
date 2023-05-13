062fe4139ffe93ded6328a66ef06fd7c1fadc43c
Alex <kid1412621@gmail.com>
2023-05-13 22:55:15 +0800

    temp remove drafts; fix dark theme on code;

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -12,40 +12,18 @@ const { page, frontmatter } = useData()
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
         <a class="text-xl" href="/" aria-label="NanoNova's Cyberspace">
-          <img
-            class="inline-block mr-2"
-            style="width: 36px; height: 31px"
-            alt="logo"
-            src="/logo.svg"
-          />
-          <span
-            v-if="!frontmatter.index"
-            class="hidden md:inline dark:text-white"
-            >NanoNova's Cyberspace</span
-          >
+          <img class="inline-block mr-2" style="width: 36px; height: 31px" alt="logo" src="/logo.svg" />
+          <span v-if="!frontmatter.index" class="hidden md:inline dark:text-white">NanoNova's Cyberspace</span>
         </a>
         <div class="text-sm text-gray-500 dark:text-white leading-5">
-          <a
-            class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="/posts/about_me"
-            target="_self"
-            rel="noopener"
-            ><span class="hidden sm:inline">About </span>me</a
-          >
+          <a class="hover:text-gray-700 dark:hover:text-gray-200" href="/posts/about_me" target="_self" rel="noopener">
+            About <span class="hidden sm:inline">me</span></a>
           <span class="mr-2 ml-2">·</span>
-          <a
-            class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="/feed.rss"
-            >RSS<span class="hidden sm:inline"> Feed</span></a
-          >
+          <a class="hover:text-gray-700 dark:hover:text-gray-200" href="/feed.rss">
+            RSS <span class="hidden sm:inline"> Feed</span></a>
           <span class="mr-2 ml-2">·</span>
-          <a
-            class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="https://github.com/kid1412621/kid1412621.github.io"
-            target="_blank"
-            rel="noopener"
-            >Github →</a
-          >
+          <a class="hover:text-gray-700 dark:hover:text-gray-200"
+            href="https://github.com/kid1412621/kid1412621.github.io" target="_blank" rel="noopener">Github →</a>
         </div>
       </nav>
     </div>
